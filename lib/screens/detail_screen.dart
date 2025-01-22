916b0122e28f2be6cedc7f61a46ad1e754825b09
Belen clase <belen_aparicio_m-tsapp1ma2324@cev.com>
2025-01-22 10:18:48 +0100

    El archivo de detail lo he usado para cuando tu entres en la lista de personajes y pulses en uno te muestre información mas especifica de este y le puedas añadir a favoritos y eliminar de favoritos

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,48 @@
+import 'package:ejercicio_fluter_of/providers/favorites_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:ejercicio_fluter_of/models/character.dart';
+import 'package:ejercicio_fluter_of/services/api_service.dart';
+class DetailScreen extends StatelessWidget {
+  final Character character;
+  const DetailScreen({Key? key, required this.character}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+	final favoritesProvider = Provider.of<FavoritesProvider>(context);
+	final isFavorite = favoritesProvider.favorites.contains(character);
+	return Scaffold(
+  	appBar: AppBar(
+    	title: Text(character.name),
+  	),
+  	body: Padding(
+    	padding: const EdgeInsets.all(16.0),
+    	child: Column(
+      	crossAxisAlignment: CrossAxisAlignment.start,
+      	children: [
+        	Text(
+          	'Nombre: ${character.name}',
+          	style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        	),
+        	SizedBox(height: 8),
+        	Text('Género: ${character.gender}', style: TextStyle(fontSize: 16)),
+        	Text('Cultura: ${character.culture}', style: TextStyle(fontSize: 16)),
+        	Text('Nacido: ${character.born}', style: TextStyle(fontSize: 16)),
+        	Text('Fallecido: ${character.died}', style: TextStyle(fontSize: 16)),
+        	Spacer(),
+        	ElevatedButton.icon(
+          	onPressed: () {
+            	if (isFavorite) {
+              	favoritesProvider.removeFavorite(character);
+            	} else {
+              	favoritesProvider.addFavorite(character);
+            	}
+          	},
+          	icon: Icon(isFavorite ? Icons.favorite : Icons.favorite_border),
+          	label: Text(isFavorite ? 'Eliminar de Favoritos' : 'Agregar a Favoritos'),
+        	),
+      	],
+    	),
+  	),
+	);
+  }
+}
