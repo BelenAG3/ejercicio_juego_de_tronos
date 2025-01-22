6c4dd76499a0750cdb430c67b1c5304f815c3484
Belen clase <belen_aparicio_m-tsapp1ma2324@cev.com>
2025-01-22 10:30:14 +0100

    En el main he configurado toda la información de la página principal para que te muestre un personaje al principio y puedas generar otro aleatriamente y a través de un menu te lleve al resto de ventanas que sera la lista de personajes y la de favoritos

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
+import 'providers/favorites_provider.dart';
+import 'screens/featured_screen.dart';
+import 'screens/list_screen.dart';
+import 'screens/favorites_screen.dart';
 void main() {
-  runApp(const MainApp());
+  runApp(
+	MultiProvider(
+  	providers: [
+    	ChangeNotifierProvider(create: (_) => FavoritesProvider()),
+  	],
+  	child: MyApp(),
+	),
+  );
 }
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+	return MaterialApp(
+  	debugShowCheckedModeBanner: false,
+  	title: 'Game of Thrones',
+  	theme: ThemeData(primarySwatch: Colors.blue),
+  	initialRoute: '/',
+  	routes: {
+    	'/': (context) => FeaturedScreen(),
+    	'/list': (context) => ListScreen(),
+    	'/favorites': (context) => FavoritesScreen(),
+  	},
+	);
   }
 }
